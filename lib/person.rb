59020cf0b51f4f0dd4a3f4e6cf8582aa67a10da6
Andrew-Ochieng <andrewomosh44@gmail.com>
2022-10-21 12:03:28 +0300

    Completed assignment

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,26 @@
 class Person
+    # setter methods
+    def name=(name)
+        @name = name
+    end
+
+    def job=(job)
+        @job = job
+    end
+
+    # getter methods
+    def name
+        @name
+    end
+
+    def job
+        @job
+    end
 
 end
+
+person1 = Person.new
+person1.name = "Ellen White"
+person1.job = "Preacher"
+puts person1.name
+puts person1.job
